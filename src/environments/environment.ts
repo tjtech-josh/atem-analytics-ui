348c9f017fa0886bb455513b7c31531fe87c9ee4
joshuawieczorek <me@joshuaw.us>
2024-08-04 18:13:47 -0400

    fix typo in file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     keycloakUrl:'https://id.tj-tech.us',
     keycloakRealm:'atem-analytics',
     keycloakClientId:'aa-chat-ui',
-    keycloakRedirectUri:'http://localhost:4200/',
+    keycloakRedirectUri:'https://atem-analytics.tj-tech.us/',
     apiUrlChatService: 'https://apis.atem-analytics.tj-tech.us/chat',
 };
